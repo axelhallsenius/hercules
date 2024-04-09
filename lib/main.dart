bb4478deee167cfebb7c9098f8d323145b54c54a
shiqi0515 <shiqi0515@outlook.com>
2024-04-09 22:22:56 +0200

    Update settings.json and GeneratedPluginRegistrant.swift

--- lib/main.dart
+++ lib/main.dart
@@ -103,77 +103,194 @@ class _MyHomePageState extends State<MyHomePage> {
 class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Center(
-          child: Column(
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: <Widget>[
+          // Category section with horizontal scrolling.
+          Wrap(
+            spacing: 20, // Setting the space between the tags
+            runSpacing: 20,
+            alignment: WrapAlignment.center,
             children: <Widget>[
-              Align(
-                alignment: Alignment.center,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width * 0.02),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'Start by typing or select tags',
-                      prefixIcon: Icon(Icons.search),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                  height:
-                      5), // Setting the height between the text field and the tags
-              Wrap(
-                spacing: 6, // Setting the space between the tags
+              Column(
+                mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: Text('Mild Pain'),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(color: Colors.black),
-                      minimumSize: Size(80, 30), // Setting the size of the tags
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Container(),
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero)),
                     ),
                   ),
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: Text('Burn'),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(color: Colors.black),
-                      minimumSize: Size(80, 30), // Setting the size of the tags
+                  Text('Obisterics'),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Container(),
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero)),
                     ),
                   ),
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: Text('Fracture'),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(color: Colors.black),
-                      minimumSize: Size(80, 30), // Setting the size of the tags
+                  Text('Gaming Injury'),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Container(),
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero)),
                     ),
                   ),
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: Text('First Aid'),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(color: Colors.black),
-                      minimumSize: Size(80, 30), // Setting the size of the tags
+                  Text('Major Owies'),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Container(),
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero)),
                     ),
                   ),
-                  OutlinedButton(
-                    onPressed: () {},
-                    child: Text('...'),
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(color: Colors.black),
-                      minimumSize: Size(80, 30), // Setting the size of the tags
+                  Text('Burn Care'),
+                ],
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Container(),
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero)),
                     ),
                   ),
+                  Text('Covid'),
                 ],
               ),
             ],
           ),
-        ),
-      ],
+          SizedBox(height: 30),
+          // Button for more categories.
+          Center(
+            child: Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  // Action to expand or view more categories.
+                },
+                child: Text('More Categories ...'),
+              ),
+            ),
+          ),
+          SizedBox(height: 30),
+          // History section title.
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.access_time),
+                SizedBox(width: 18),
+                Text('History', style: Theme.of(context).textTheme.titleLarge),
+              ],
+            ),
+          ),
+          SizedBox(height: 30),
+          // History section with scrollable cards or tiles.
+          Wrap(
+            spacing: 6, // Setting the space between the tags
+            alignment: WrapAlignment.center,
+            children: <Widget>[
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Handling blisters'),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.black),
+                  minimumSize: Size(80, 30), // Setting the size of the tags
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Talking to Newborns'),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.black),
+                  minimumSize: Size(80, 30), // Setting the size of the tags
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Article Name'),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.black),
+                  minimumSize: Size(80, 30), // Setting the size of the tags
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Rheumatism'),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.black),
+                  minimumSize: Size(80, 30), // Setting the size of the tags
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Small Owie'),
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.black),
+                  minimumSize: Size(80, 30), // Setting the size of the tags
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.zero,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
